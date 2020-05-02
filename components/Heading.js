538e924ebb3df82a5bb8fc67ddd443bc235b2045
Mo <56743114+momakes@users.noreply.github.com>
2020-05-02 17:09:59 +0430

    Add styling and data

---
+++ components/Heading.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Heading = styled.div`
+  font-size: 19px;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+`;
+
+export default Heading;
