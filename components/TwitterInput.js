9900cce6f09b5dd39d5a17b9c752dc7a472152a6
Mo <56743114+momakes@users.noreply.github.com>
2020-05-02 17:23:18 +0430

    Add buttons

---
+++ components/TwitterInput.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const TwitterInput = styled.input`
+  font-size: 19px;
+  padding: 5px;
+  border: 1px solid #eee;
+
+  display: inline-block;
+  width: 200px;
+  margin: 0;
+  margin-right: 10px;
+
+  ::placeholder {
+    color: #999;
+  }
+`;
+
+export default TwitterInput;
